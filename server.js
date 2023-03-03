f30ac47c9c747e56491c51726f0640f3852b0e4c
Yashneet kalra <YASHNEET_754@vipsedu.in>
2023-03-03 13:14:47 +0530

    commented the DB files

--- server.js
+++ server.js
@@ -9,23 +9,23 @@ const toDoList = todosModule.toDoList;
 app.use("/todos/", todosModule.router)
 
 // for DB APIs using ORM
-const todosDBModule = require("./routes/todos_with_db");
-app.use("/db/todos", todosDBModule.router);
+// const todosDBModule = require("./routes/todos_with_db");
+// app.use("/db/todos", todosDBModule.router);
 
 // for inititalising the DB Models
-const db = require("./db-configurations/db-config");
-db.sequelize.sync({force: false})
-    .then(() => {
-        console.log("Synced DB.");
-    }).catch((err) => {
-        console.log("Failed to sync DB : " + err.message);
-    });
+// const db = require("./db-configurations/db-config");
+// db.sequelize.sync({force: false})
+//     .then(() => {
+//         console.log("Synced DB.");
+//     }).catch((err) => {
+//         console.log("Failed to sync DB : " + err.message);
+//     });
 
 
 // for DB connection using DRIVER Library
-const dbDriver = require("./pg-promise-db-configurations/pg-promise-db-configs");
-const todosDBDriverModule = require("./routes/todos_with_driver");
-app.use("/db/driver/", todosDBDriverModule);
+// const dbDriver = require("./pg-promise-db-configurations/pg-promise-db-configs");
+// const todosDBDriverModule = require("./routes/todos_with_driver");
+// app.use("/db/driver/", todosDBDriverModule);
 
 
 app.set("view engine", "ejs")
